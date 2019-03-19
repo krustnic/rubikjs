754019cc6271184f1aea09b06b2aca9826bfad41
avmityakov <avmityakov@gmail.com>
2019-03-19 15:32:10 +0300

    webcam modal component

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="test">
-    <FridrihView></FridrihView>
+    <!-- <FridrihView></FridrihView> -->
+    <WebcamModal :show="isShow" @close="isShow=false"></WebcamModal>
+    <button @click="isShow=!isShow">Show</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import FridrihView from '@/components/FridrihView/Main.vue';
+import WebcamModal from '@/components/WebcamModal.vue';
 
 @Component({
   components: {
     FridrihView,
+    WebcamModal,
   },
 })
-export default class extends Vue {}
+export default class extends Vue {
+  isShow: boolean = false;
+}
 </script>
