7e5317597ac54789df19c5f8faa376b34afb18a7
avmityakov <avmityakov@gmail.com>
2019-03-20 16:36:58 +0300

    add build folder

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  outputDir: 'docs',
   chainWebpack: config => {
     const defaultState = process.env.VUE_APP_DEFAULT_STATE; // mock or prod
     config.resolve.alias.set(
